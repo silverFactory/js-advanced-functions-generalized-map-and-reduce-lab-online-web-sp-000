d44bcf4e1bc8f44727db2601ee792f84ec3882b2
Will Studabaker <will.studabaker@gmail.com>
2021-02-11 14:36:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your functions here
+function map(array, callback){
+  let newArray = []
+  for (const element of array){
+    newArray.push(callback(element))
+  }
+  return newArray
+}
+// function reduce(array, callback, startingPoint = 0){
+//   let total = startingPoint
+//   let truthiness
+//   for (const element of array){
+//     if ((callback(element, total)) === true || (callback(element, total)) === false){
+//       truthiness = callback(element, total)
+//     } else {
+//       total = callback(element, total)
+//     }
+//  }
+//  if (total > 0){
+//    return total
+//  } else {
+//    return truthiness
+//  }
+// }
+function reduce(array, callback, startingPoint = 0){
+  let total = startingPoint
+  if (typeof callback(1, true) == "boolean"){
+    total = true
+  }
+  for (const element of array){
+    total = callback(element, total)
+  }
+  return total
+}
